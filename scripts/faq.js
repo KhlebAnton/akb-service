503aa68001ac699c5f2acc8f8e9f537e837b5965
Anton Khleb <khleb.4nton@yandex.ru>
2025-07-09 20:28:11 +0500

    add fq and footer

---
+++ scripts/faq.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const faqItems = document.querySelectorAll('.faq-item');
+    
+    faqItems.forEach(item => {
+        
+        
+       item.addEventListener('click', () => {
+            // Закрываем все открытые элементы, кроме текущего
+            faqItems.forEach(otherItem => {
+                if (otherItem !== item && otherItem.classList.contains('is-open')) {
+                    otherItem.classList.remove('is-open');
+                }
+            });
+            
+            // Переключаем текущий элемент
+            item.classList.toggle('is-open');
+        });
+    });
+});
